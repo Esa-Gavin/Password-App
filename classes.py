c5c7937ad3f6ec80e8a82fda1c30e96fc172d62a
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-24 23:22:51 +0300

    add the various classmethods

--- classes.py
+++ classes.py
@@ -42,3 +42,33 @@ class Credentials:
 
         Credentials.credentials_list.remove(self)
 
+    
+    @classmethod
+    def display_credentials(cls):
+        ''' display_credentials method that displays objects in the credentials list '''
+
+        return cls.credentials_list
+
+    @classmethod
+    def find_by_account(cls, account):
+        ''' find_by_account method that searches objects in the credentials list by account '''
+
+        for credentials in cls.credentials_list:
+            if credentials.account == account:
+                return credentials
+
+    @classmethod
+    def username_exists(cls, username):
+        ''' username_exists method that checks if a username exists '''
+
+        for credentials in cls.credentials_list:
+            if credentials.username == username:
+                return True
+        
+        return False
+
+    @classmethod
+    def copy_password(cls, account):
+        found_credentials = Credentials.find_by_account(account)
+        pyperclip.copy(found_credentials.password)
+
