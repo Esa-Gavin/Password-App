3bf29725fefb387b31c29462bfc49bd204c8f9fc
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-24 23:10:44 +0300

    test_save_user test case to check whether a new user has been saved into the user list

--- classes_test.py
+++ classes_test.py
@@ -17,3 +17,12 @@ class TestUser(unittest.TestCase):
         '''
         self.assertEqual(self.new_user.username, "genichiro21")
         self.assertEqual(self.new_user.password, "password")
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to check whether a new user has been saved into the user list
+
+        '''
+
+        self.new_user.save_user()  # saving the new user
+        self.assertEqual(len(User.user_list), 1)
