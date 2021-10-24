5dfd87b01afc390e1e75537fb2b70f2fcf1900aa
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-24 23:09:13 +0300

    create a method setUp

--- classes_test.py
+++ classes_test.py
@@ -2,4 +2,10 @@ from classes import User, Credentials  # importing both of my classes
 import unittest
 
 class TestUser(unittest.TestCase):
-    
+
+    def setUp(self) -> None:
+        '''
+        set up method to run before each test cases
+
+        '''
+        self.new_user = User("genichiro21", "password")
