3f4ddea4ff599abac5e516cd5e23807cefb1b27b
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-24 23:01:59 +0300

    create a class user, initilialize the properties, also create a method for saving

---
+++ classes.py
@@ -0,0 +1,17 @@
+import pyperclip
+
+class User:
+    '''class that generates new instances of users'''
+
+    user_list = [] #empty user list
+
+    def __init__(self, username, password) -> None:
+        '''blueprint that defines the properties of a user'''
+
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        '''save_user method saves contact objects into user_list'''
+        
+        User.user_list.append(self)
